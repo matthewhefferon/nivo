e6f724c72b950d0a307baa15ab2655431a51d441
Matthew Hefferon <matthefferon@gmail.com>
2021-11-04 19:45:57 -0600

    Line

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import Line from './components/Pie';
+import Pie from './components/Pie';
 import Bar from './components/Bar';
+import Line from './components/Line';
 
 function App() {
   return (
     <div style={{ height: 500 }} className="App">
-      <Line />
+      <Pie />
       <Bar />
+      <Line />
     </div>
   );
 }
